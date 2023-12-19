553ce76e3a84e600f1db4f652dad984db97c5409
masum184e <masum184e@gmail.com>
2023-12-20 02:46:56 +0600

    bookey added

--- frontend/src/routes/router.jsx
+++ frontend/src/routes/router.jsx
@@ -2,18 +2,22 @@ import { createBrowserRouter } from "react-router-dom";
 import Root from "../components/Root";
 import SSLCOMERZ from "./../methods/SSLCOMERZ"
 import StripeCheckout from "../methods/StripeCheckout";
+import Bookey from "../methods/Bookey";
 
 const router = createBrowserRouter([
   {
-    path:"/",
-    element:<Root />,
-    children:[
+    path: "/",
+    element: <Root />,
+    children: [
       {
-        path:"/sslcomerz",
-        element:<SSLCOMERZ />
-      },{
-        path:"/stripe-checkout",
-        element:<StripeCheckout />
+        path: "/sslcomerz",
+        element: <SSLCOMERZ />
+      }, {
+        path: "/stripe-checkout",
+        element: <StripeCheckout />
+      }, {
+        path: "/bookey",
+        element: <Bookey />
       }
     ]
   }
